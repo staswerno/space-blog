3727f09c28f318e7c576e1a085a5d2f77cc5a33c
Staś Werno <89650713+staswerno@users.noreply.github.com>
2021-11-29 12:32:14 +0100

    add initial layout

---
+++ src/ContentfulData.js
@@ -0,0 +1,15 @@
+import { Box, Typography, Paper } from '@mui/material';
+
+export default function ContentfulDataComp () {
+    return (
+        <>
+        <Box m={2}>
+        <Paper>
+        <Typography>
+            Contentful Data Here!
+        </Typography>
+        </Paper>
+        </Box>
+        </>
+    )
+}
