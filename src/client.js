dc49896d04bf0ae60b49068955d3516a6dc415d0
garciamarin <gsosa_97@hotmail.com>
2021-12-01 20:46:04 +0000

    pull Stas and Dyvias Updates

--- src/client.js
+++ src/client.js
@@ -1,6 +1,8 @@
 import * as contentful from "contentful";
 
 export const client = contentful.createClient({
-  space: process.env.REACT_APP_SPACE_ID,
-  accessToken: process.env.REACT_APP_ACCESS_TOKEN,
+  space: 'xboxdo2c4vb8', 
+  // process.env.REACT_APP_SPACE_ID,
+  accessToken: 'pfc78Qz8o8aMdb9pSRdqpHBVgjtNRVMXQ-e-6CiRgYg'
+  //process.env.REACT_APP_ACCESS_TOKEN,
 });
