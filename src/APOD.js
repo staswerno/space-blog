70602475126d805bd26b41f935c18e8a052d6f17
Staś Werno <89650713+staswerno@users.noreply.github.com>
2021-12-12 12:35:05 +0100

    add apod loading and error handling functionality

--- src/APOD.js
+++ src/APOD.js
@@ -1,21 +1,20 @@
-import { Typography, Link } from "@mui/material";
+import { Typography, Link, Fade } from "@mui/material";
 import { useState, useEffect } from "react";
 
 export default function APOD() {
 	const [pic, setPic] = useState({});
 	const [isLoading, setIsLoading] = useState(true);
 	const [isError, setIsError] = useState(false);
+	const nasaUrl = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_KEY}`;
 
 	useEffect(() => {
-		fetch(
-			"https://api.nasa.gov/planetary/apod?api_key=7DEpjfAdNvLrVQayzTp0VKRpzXAgGVBMismCL6r2"
-		)
+		fetch(nasaUrl)
 			.then((response) => response.json())
 			.then((json) => {
 				setIsLoading(false);
 				setPic(json);
 			})
-			.catch(() => console.log("request failed"));
+			.catch(() => setIsError(true));
 	}, []);
 
 	console.log(pic.hdurl);
@@ -30,10 +29,29 @@ export default function APOD() {
 			>
 				Astronomy Picture of the Day
 			</Typography>
-
-			<Link href="https://apod.nasa.gov/apod/astropix.html" target="_blank">
-				<img width="100%" src={pic.hdurl} alt="astronomy picture of the day" />
-			</Link>
+			{isLoading ? (
+				<Fade in={true} timeout={700}>
+					<Typography textAlign="center" variant="body2">
+						loading...
+					</Typography>
+				</Fade>
+			) : isError ? (
+				<Fade in={true} timeout={700}>
+					<Typography textAlign="center" variant="body2">
+						nasa's api is not responding. please refresh or try again later
+					</Typography>
+				</Fade>
+			) : (
+				<Fade in={true} timeout={700}>
+					<Link href="https://apod.nasa.gov/apod/astropix.html" target="_blank">
+						<img
+							width="100%"
+							src={pic.url}
+							alt="astronomy picture of the day"
+						/>
+					</Link>
+				</Fade>
+			)}
 			<Typography textAlign="center" mt={2}>
 				<Link
 					href="https://apod.nasa.gov/apod/astropix.html"
